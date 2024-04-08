61f55b63b8bed5f0df02c602fb2d3fb55519ffbc
nin <95270582+ninsaccplayzyt@users.noreply.github.com>
2024-04-08 20:48:44 +1000

    Update bacle.py

--- bacle/bacle.py
+++ bacle/bacle.py
@@ -1,8 +1,7 @@
 from discord.ext import commands
 
-
-class Banana(commands.Cog):
-    """Reacts with the bacle emoji if someone says bacle."""
+class Bacle(commands.Cog):
+    """Reacts with a bacle emoji if someone says bacle."""
 
     def __init__(self, bot):
         self.bot = bot
@@ -10,10 +9,10 @@ class Banana(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if "BACLE" in message.content.upper():
+            # Replace '1214520769011388436' with the ID of your custom emoji
             emoji_id = 1214520769011388436
             custom_emoji = f"<:bacle:{emoji_id}>"
             await message.add_reaction(custom_emoji)
 
-
 def setup(bot):
-    bot.add_cog(Banana(bot))
+    bot.add_cog(Bacle(bot))
